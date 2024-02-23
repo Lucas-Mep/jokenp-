8c8d14e24c7f178c5fa8d084096d595eff37d827
Lucas-Mep <lucasclifforddepaula@gmail.com>
2024-02-23 10:40:54 -0300

    Jokenpô

---
+++ script.js
@@ -0,0 +1,40 @@
+const result = document.querySelector(".result")
+const humanScore = document.querySelector("#human-score")
+const machineScore = document.querySelector("#machine-score")
+
+
+let humanScoreNumber = 0
+let machineScoreNumber = 0
+
+const playHuman = (humanChoice) => {
+  playTheGame(humanChoice, playMachine());
+};
+
+const playMachine = () => {
+  const choices = ["rock", "paper", "scissor"];
+  const randomNumber = Math.floor(Math.random() * 3);
+
+  console.log(randomNumber);
+
+  return choices[randomNumber];
+};
+
+const playTheGame = (human, machine) => {
+  console.log("Humano: " + human, "Machine: " + machine);
+
+  if (human === machine) {
+    result.innerHTML = "Deu empate";
+  } else if (
+    (human === "paper" && machine === "rock") ||
+    (human === "rock" && machine === "scissor") ||
+    (human === "scissor" && machine === "paper")
+  ) {
+    humanScoreNumber++
+    humanScore.innerHTML = humanScoreNumber
+    result.innerHTML = "Você ganhou!";
+  } else {
+    machineScoreNumber++
+    machineScore.innerHTML = machineScoreNumber
+    result.innerHTML = "Você perdeu da Alexa";
+  }
+};
